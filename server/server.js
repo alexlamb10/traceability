e33ecdc82f8cf3de5c3ac69bebdfa22b6341d3da
Alex Lamb <alexlamb104@gmail.com>
2022-03-17 15:24:52 -0600

    Added a box with ability to add info from user, as well as added rollbar

--- server/server.js
+++ server/server.js
@@ -6,6 +6,17 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '996054aa80644c54829e2a3d26cfcfe7',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.use(express.static(path.join(__dirname, "../public")))
 
 const port = process.env.PORT || 5055
