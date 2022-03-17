16873b1f25c199a1d44541b7a3d41f4fd507a730
Alex Lamb <alexlamb104@gmail.com>
2022-03-17 14:40:41 -0600

    Attempting deploy

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const path = require("path")
 const app = express()
 app.use(express.json())
 
-app.use(express.static((path.join(__dirname, "/index.html"))))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
